45fc165887097e2063c197fc793fa45b8bf95afc
Ming Bao <108658725+Ming-Bao@users.noreply.github.com>
2024-06-07 19:02:35 +1200

    Add files via upload

---
+++ rename.py
@@ -0,0 +1,13 @@
+import os
+
+name = input("Enter name of file:")
+offset = int(input("What episode to start at:"))
+fileExtention = input("What is the file Extention:")
+currentFilename = os.path.basename(__file__)
+
+files = [f for f in os.listdir() if os.path.isfile(f) and f != currentFilename]
+files.sort
+numPad = len(str(len(files)))
+
+for i in range(0, len(files)):
+    os.rename(files[i], f"{name} E{str(i+offset).zfill(numPad)}.{fileExtention}")
